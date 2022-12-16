c67b9a33d934cec26175f170b21e45285c084efa
Aman <Amandubey451@gmail.com>
2022-12-16 16:51:36 +0530

    Pace Stock Assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Signup from './components/Signup';
+import News from './components/News';
+import {Route,Routes,BrowserRouter} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+       <BrowserRouter>
+       <Routes>
+         <Route path="/" element={<Signup/>}/>
+         <Route path="/News" element={<News/>}/>
+       </Routes>
+       </BrowserRouter>
+  )
 }
 
 export default App;
